5180050ca9a14a8882c1482c335fdfe8fb906579
Duder63 <gary.shew.email@gmail.com>
2017-01-24 11:39:49 -0500

    commented out unused functions in main.js

--- js/main.js
+++ js/main.js
@@ -61,15 +61,15 @@ function main() {
         offset: 80
     })
 
-  	$(document).ready(function() {
-  	    $("#testimonial").owlCarousel({
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-        });
+  	// $(document).ready(function() {
+  	//     $("#testimonial").owlCarousel({
+   //      navigation : false, // Show next and prev buttons
+   //      slideSpeed : 300,
+   //      paginationSpeed : 400,
+   //      singleItem:true
+   //      });
 
-  	});
+  	// });
 
   	// Portfolio Isotope Filter
     $(window).load(function() {
@@ -112,9 +112,9 @@ function main() {
   initParallax();
 
   	// Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});	
+	// $("a[rel^='prettyPhoto']").prettyPhoto({
+	// 	social_tools: false
+	// });	
 
 }());
 

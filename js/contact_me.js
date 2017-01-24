4b207220574e4cbf8745f7ed675e53ae43125b27
Duder63 <gary.shew.email@gmail.com>
2017-01-24 10:03:29 -0500

    email form

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,7 +17,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                //url: "././mail/contact_me.php",
+                url: "http://formspree.io/gary.shew.email@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
